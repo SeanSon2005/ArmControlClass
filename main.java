1ccc4ebc4ba336582f6df2d51c4015bcd9128fdc
Sean Son <astroson2005@gmail.com>
2023-01-09 15:50:29 -0800

    first commit

---
+++ main.java
@@ -0,0 +1,7 @@
+public class main {
+    public static void main(String[]args){
+        ArmControl arm = new ArmControl(100, 50, 50);
+        arm.calcAngles(50, 90, 0);
+        System.out.println(arm.getFirstAngle() + " " + arm.getSecondAngle() + " " + arm.getTurretAngle());
+    }
+}
